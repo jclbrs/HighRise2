4fb2250aca91929c983d2433c18950172bc9047d
Joe Calabrese <joe@ceruleangames.com>
2021-08-03 10:59:40 -0400

    Added calculations for various stability scenarios

--- Assets/Scripts/SimulationLogic/LandingZoneLogic.cs
+++ Assets/Scripts/SimulationLogic/LandingZoneLogic.cs
@@ -169,7 +169,7 @@ namespace Assets.Scripts.SimulationLogic
 
 		// Once a dropping piece gets a collision, it was added to the BuildArea (done elsewhere)
 		// Then this is run to determine if the new structure is stable
-		public bool CalculateStability() // joe, maybe return bool. or perhaps object detailing stability details
+		public bool CalculateStability()
 		{
 			bool isStable = false;
 
@@ -197,15 +197,147 @@ namespace Assets.Scripts.SimulationLogic
 						return false;
 				}
 			}
+			// This row passed, so the "current row forces" are now the "above row forces", for the next row
+			_aboveRowForces.Clear();
+			_aboveRowForces.AddRange(_currentRowForces);
+			_currentRowForces.Clear();
 			return true;
 		}
 
 		private bool CalculatePieceOnRow(int row, ref int col)
 		{
 			// block is present
+			int currentCol = col;
 			RowPieceForce rowPieceForce = new RowPieceForce();
-			rowPieceForce.StartColIdx = col;
+			rowPieceForce.StartColIdx = currentCol;
 			// Find how wide the piece is on this row
+			int nextCol = CalcRowForceStartAndEndIndex(row, currentCol, ref rowPieceForce);
+			col = nextCol; // this is passed back to the caller by reference
+			rowPieceForce.Mass = rowPieceForce.EndColIdx - rowPieceForce.StartColIdx + 1; // Each block has a weight of 1.  So a piece that is 3 wide on this row has a weight = 3
+
+			// note: weight of a block is coincidentally the same as the width of the block, so can use it in the equation
+			//       if we change that, the code will need to be adjusted
+			int rowPieceWidth = rowPieceForce.EndColIdx - rowPieceForce.StartColIdx + 1;
+			rowPieceForce.CtrOfMass = rowPieceForce.StartColIdx + rowPieceWidth / 2f;
+			// Now find if there are accumulated pieces above that have a center of gravity on this current piece
+			CalcEffectOfAboveForces(rowPieceForce);
+
+			// === Run stability checks ====
+			// Check if already on the ground
+			if (row == 0)
+			{
+				_currentRowForces.Add(rowPieceForce);
+				return true;
+			}
+
+			// Check if exactly on the border
+			// note: since pieces can only be up to 3 blocks wide, this means that the piece's blocks on this row must be 2 blocks wide
+			int accumulatedCtrOfMassAsInt = Convert.ToInt32(rowPieceForce.AccumulatedCtrOfMass);
+			if (rowPieceForce.AccumulatedCtrOfMass == accumulatedCtrOfMassAsInt)
+			{
+				// yes, it is on the border.  Now determine what is below it, on left and right
+				// note: because of the geometric limitations of 3 blocks max, there must be at least 1 block below it (left or right)
+				int pieceIdBelowLeft = LandingZone[accumulatedCtrOfMassAsInt - 1, row - 1].PieceId;
+				int pieceIdBelowRight = LandingZone[accumulatedCtrOfMassAsInt, row - 1].PieceId;
+				bool hasPieceBelowLeft = (pieceIdBelowLeft > int.MinValue);
+				bool hasPieceBelowRight = (pieceIdBelowRight > int.MinValue);
+				if (hasPieceBelowLeft && hasPieceBelowRight) // There are piece blocks below on left and right.  
+				{
+					if (pieceIdBelowLeft == pieceIdBelowRight)
+					{
+						// The blocks on left and right are part of the same piece, so leave this current piece's center of mass as is
+						_currentRowForces.Add(rowPieceForce);
+						return true;
+					}
+
+					// The blocks below are from different pieces, so split this piece's center of mass as 2 separate forces, directly on center of below left and below right
+					RowPieceForce splitForceOnLeft = SplitRowForceIntoTwo(ref rowPieceForce);
+					_currentRowForces.Add(rowPieceForce);
+					_currentRowForces.Add(splitForceOnLeft);
+					return true;
+				}
+				else // there is only 1 block beneath this piece.  We will allow stability here, by moving the center of mass slightly within the lower block's range
+				{
+					float shiftCenterOfMass = (hasPieceBelowLeft ? -0.1f : 0.1f);
+					rowPieceForce.AccumulatedCtrOfMass += shiftCenterOfMass;
+					_currentRowForces.Add(rowPieceForce);
+					return true;
+				}
+			}
+
+
+			// Check if above another piece
+			int colIdxWithCtrOfGrav = (int)Math.Floor(rowPieceForce.AccumulatedCtrOfMass);
+			if (LandingZone[colIdxWithCtrOfGrav, row - 1].PieceId > int.MinValue)
+			{
+				_currentRowForces.Add(rowPieceForce);
+				return true;
+			}
+			else // not above another piece.  This means either it is straddling 2 pieces below, or its center is not stable
+			{
+				// check if straddling, which can only happen if the piece is 3-blocks wide
+				bool is3BlocksWide = (rowPieceForce.EndColIdx - rowPieceForce.StartColIdx + 1 == 3);
+				bool hasBelowLeftPiece = (LandingZone[currentCol, row - 1].PieceId > int.MinValue);
+				bool hasBelowRightPiece = (LandingZone[currentCol + 2, row - 1].PieceId > int.MinValue);
+				if (is3BlocksWide && hasBelowLeftPiece && hasBelowRightPiece)
+				{
+					List<RowPieceForce> straddleAdjustedPieces = CalcStraddledForces(rowPieceForce);
+					_currentRowForces.AddRange(straddleAdjustedPieces);
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return false; // the code shouldn't get here, as hopefully, all cases are covered above
+		}
+
+		private List<RowPieceForce> CalcStraddledForces(RowPieceForce rowPieceForce)
+		{
+			RowPieceForce leftForce = new RowPieceForce();
+			RowPieceForce rightForce = new RowPieceForce();
+
+			leftForce.StartColIdx = rowPieceForce.StartColIdx;
+			leftForce.EndColIdx = rowPieceForce.StartColIdx;
+			rightForce.StartColIdx = rowPieceForce.EndColIdx;
+			rightForce.EndColIdx = rowPieceForce.EndColIdx;
+			leftForce.Mass = 1f;
+			rightForce.Mass = 1f;
+			leftForce.MassAbove = rowPieceForce.MassAbove / 2f;
+			rightForce.MassAbove = rowPieceForce.MassAbove / 2f;
+			leftForce.AccumulatedMass = leftForce.Mass + leftForce.MassAbove;
+			rightForce.AccumulatedMass = rightForce.Mass + rightForce.MassAbove;
+			leftForce.CtrOfMass = leftForce.StartColIdx + 0.5f;
+			rightForce.CtrOfMass = rightForce.StartColIdx + 0.5f;
+			leftForce.CtrOfMassAbove = leftForce.CtrOfMass;
+			rightForce.CtrOfMassAbove = rightForce.CtrOfMass;
+			leftForce.AccumulatedCtrOfMass = leftForce.CtrOfMass;
+			rightForce.AccumulatedCtrOfMass = rightForce.CtrOfMass;
+			List<RowPieceForce> straddledForces = new List<RowPieceForce>();
+			straddledForces.Add(leftForce);
+			straddledForces.Add(rightForce);
+			return straddledForces;
+		}
+
+		private void CalcEffectOfAboveForces(RowPieceForce rowPieceForce)
+		{
+			RowPieceForce aboveRowForce = _aboveRowForces.Find(p => p.CtrOfMass >= rowPieceForce.StartColIdx && p.CtrOfMass <= rowPieceForce.EndColIdx);
+			if (aboveRowForce != null)
+			{
+				rowPieceForce.CtrOfMassAbove = aboveRowForce.AccumulatedCtrOfMass;
+				rowPieceForce.MassAbove = aboveRowForce.AccumulatedMass;
+			}
+			// now calculate the total weight and center of gravity for this piece, including the effect of the pieces weighing down on it
+			rowPieceForce.AccumulatedMass = rowPieceForce.Mass + rowPieceForce.MassAbove; // Joe, this could be a calculated property in RowPieceForce class
+			rowPieceForce.AccumulatedCtrOfMass = (rowPieceForce.Mass * rowPieceForce.CtrOfMass + rowPieceForce.MassAbove * rowPieceForce.CtrOfMassAbove) / rowPieceForce.AccumulatedMass;
+			// We're done with the above forces on this piece, so it can be removed
+			_aboveRowForces.Remove(aboveRowForce);
+			_currentRowForces.Add(rowPieceForce);
+		}
+
+		private int CalcRowForceStartAndEndIndex(int row, int col, ref RowPieceForce rowPieceForce)
+		{
 			rowPieceForce.EndColIdx = col;
 			for (int i = 0; i + col < 5; i++)
 			{
@@ -215,41 +347,36 @@ namespace Assets.Scripts.SimulationLogic
 					break;
 			}
 			col = rowPieceForce.EndColIdx + 1;
-			rowPieceForce.Weight = rowPieceForce.EndColIdx - rowPieceForce.StartColIdx + 1; // Each block has a weight of 1.  So a piece that is 3 wide on this row has a weight = 3
+			return col;
+		}
 
-			// note: weight of a block is coincidentally the same as the width of the block, so can use it in the equation
-			//       if we change that, the code will need to be adjusted
-			int rowPieceWidth = rowPieceForce.EndColIdx - rowPieceForce.StartColIdx + 1;
-			rowPieceForce.CtrOfGrav = rowPieceForce.StartColIdx + rowPieceWidth/2f;
-			// Now find if there are accumulated pieces above that have a center of gravity on this current piece
-			RowPieceForce aboveRowForce = _aboveRowForces.Find(p => p.CtrOfGrav >= rowPieceForce.StartColIdx && p.CtrOfGrav <= rowPieceForce.EndColIdx);
-			if (aboveRowForce != null)
-			{
-				rowPieceForce.CtrOfGravAbove = aboveRowForce.AccumulatedCtrOfGrav;
-				rowPieceForce.WeightAbove = aboveRowForce.AccumulatedWeight;
-			}
-			// now calculate the total weight and center of gravity for this piece, including the effect of the pieces weighing down on it
-			rowPieceForce.AccumulatedWeight = rowPieceForce.Weight + rowPieceForce.WeightAbove; // Joe, this could be a calculated property in RowPieceForce class
-			rowPieceForce.AccumulatedCtrOfGrav =  (rowPieceForce.Weight * rowPieceForce.CtrOfGrav + rowPieceForce.WeightAbove * rowPieceForce.CtrOfGravAbove) / rowPieceForce.AccumulatedWeight;
-			// We're done with the above forces on this piece, so it can be removed
-			_aboveRowForces.Remove(aboveRowForce);
-			_currentRowForces.Add(rowPieceForce);
-			// Run stability checks
-			if (row == 0) // on the ground
-				return true;
-			// Check if above another piece
-			// (joe will need to consider if exactly between 2 lower pieces, will need to decide whether to split into two forces)
-			int colIdxWithCtrOfGrav = (int)Math.Floor(rowPieceForce.AccumulatedCtrOfGrav);
-			if (LandingZone[colIdxWithCtrOfGrav, row-1].PieceId == int.MinValue)
-			{
-				// there is no piece below, need to consider 2 cases:
-				// 1. straddling - in that case split the piece into 2 pieceRowForces and split the weight and adjust the CtrOfGravities to the center of each lower piece
-				// 2. right on the edge of one piece.  In that case, shift it to just within the lower piece
-				// Joe, but for now, just return false
-				return false;
-			}
+		private static RowPieceForce SplitRowForceIntoTwo(ref RowPieceForce rowPieceForce)
+		{
+			RowPieceForce splitForceOnLeft = new RowPieceForce();
 
-			return true;
+			splitForceOnLeft.Mass = rowPieceForce.Mass / 2f;
+			rowPieceForce.Mass /= 2f;
+
+			splitForceOnLeft.MassAbove = rowPieceForce.MassAbove / 2f;
+			rowPieceForce.MassAbove /= 2f;
+
+			splitForceOnLeft.StartColIdx = rowPieceForce.StartColIdx;
+			rowPieceForce.StartColIdx += 1;
+
+			splitForceOnLeft.EndColIdx = rowPieceForce.StartColIdx;
+
+			splitForceOnLeft.CtrOfMass = rowPieceForce.CtrOfMass - 0.5f; // possible since piece is on border, and a block width == 1
+			rowPieceForce.CtrOfMass += 0.5f;
+
+			splitForceOnLeft.AccumulatedMass = rowPieceForce.AccumulatedMass / 2f;
+			rowPieceForce.AccumulatedMass /= 2f;
+
+			splitForceOnLeft.AccumulatedCtrOfMass = splitForceOnLeft.CtrOfMass;
+			rowPieceForce.AccumulatedCtrOfMass = rowPieceForce.CtrOfMass;
+
+			splitForceOnLeft.CtrOfMassAbove = splitForceOnLeft.CtrOfMass;
+			rowPieceForce.CtrOfMassAbove = rowPieceForce.CtrOfMass;
+			return splitForceOnLeft;
 		}
 	}
 }
--- Assets/Scripts/SimulationLogic/Models/RowPieceForce.cs
+++ Assets/Scripts/SimulationLogic/Models/RowPieceForce.cs
@@ -11,11 +11,11 @@ namespace Assets.Scripts.SimulationLogic.Models
 	{
 		public int StartColIdx;
 		public int EndColIdx;
-		public float Weight; // each block has a weight of 1 (so if 2 blocks from a piece are on that row, then the weight is 2)
-		public float CtrOfGrav; // the center of gravity of the portion of the piece on this row
-		public float WeightAbove; // The calculated weight on the center of gravity of all the pieces above, resting on the current one
-		public float CtrOfGravAbove;
-		public float AccumulatedWeight; // includes all of the weights above, aka: = Weight + WeightAbove
-		public float AccumulatedCtrOfGrav; // The center of gravity, including the forces from above
+		public float Mass; // each block has a weight of 1 (so if 2 blocks from a piece are on that row, then the weight is 2)
+		public float CtrOfMass; // the center of mass of the portion of the piece on this row
+		public float MassAbove; // The calculated weight on the center of gravity of all the pieces above, resting on the current one
+		public float CtrOfMassAbove;
+		public float AccumulatedMass; // includes all of the weights above, aka: = Weight + WeightAbove
+		public float AccumulatedCtrOfMass; // The center of mass, including the forces from above
 	}
 }
